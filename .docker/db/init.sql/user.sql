7df16048c8ef6faf1d6060c883d55e243b9efb81
cyrhades <cyrhades76@gmail.com>
2024-12-19 17:03:51 +0100

    Bdd de correction

--- .docker/db/init.sql/user.sql
+++ .docker/db/init.sql/user.sql
@@ -1,15 +1,13 @@
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user` (
+  `id` int NOT NULL,
   `username` varchar(40) NOT NULL,
   `email` varchar(250) NOT NULL,
   `password` varchar(250) NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+  `otp_secret` varchar(40) DEFAULT NULL,
+  `otp_enable` int NOT NULL DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 INSERT INTO `user` ( `username`, `email`, `password`) VALUES
 ('Admin', 'admin@devsocialnetwork.com', '$2b$10$SOm3kYv4SPbR0YWVUdy48rL6zG7u9FY5fdnDf8JWSfH/75IJjT95O');
